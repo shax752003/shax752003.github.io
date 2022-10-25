ff9ce59a32c18e997396e9d660130ec1ff2c51b0
shax752003 <112305656+shax752003@users.noreply.github.com>
2022-10-25 17:51:23 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 project = document.getElementById('project');
 async function getProjects() {
-    const data = await fetch('http://127.0.0.1:5500/assets/js/projects.json');
+    const data = await fetch('https://raw.githubusercontent.com/shax752003/shax752003.github.io/main/assets/js/projects.json');
     const obj = await data.json()
     for (let i = 0; i < obj.length; i++) {
         const element = obj[i];
